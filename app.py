0d591b8d5663dd7105861cb6a165f9bdac4a9429
Herald <herald@debian.thinkpad>
2023-10-10 21:35:29 +0700

    flask setup for deployment

--- app.py
+++ app.py
@@ -1,1 +1,24 @@
-# OTP 5AE1-E34C
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+from flask import Flask, render_template, request, jsonify
+from pymongo import MongoClient
+
+# untuk membaca file .env
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+MONGODB_URI = os.environ.get("MONGODB_URI")
+DB_NAME = os.environ.get("DB_NAME")
+
+client = MongoClient(MONGODB_URI)
+
+db = client[DB_NAME]
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run('0.0.0.0', port=5000, debug=True)
